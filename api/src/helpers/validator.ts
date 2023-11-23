09eaa0233e4e874f4ac1a5aee7f1449f3ed320bf
yk-graph <yokokura.tatsuya@gmail.com>
2023-11-23 14:14:08 +0900

    fix: パスワードのバリデーション 8文字 -> 4文字

--- api/src/helpers/validator.ts
+++ api/src/helpers/validator.ts
@@ -5,12 +5,12 @@ export const registerValidator = [
   body('email').notEmpty().withMessage('メールアドレスを入力してください'),
   body('email').isEmail().withMessage('メールアドレスの形式が不正です'),
   body('password').notEmpty().withMessage('パスワードを入力してください'),
-  body('password').isLength({ min: 8 }).withMessage('パスワードは8文字以上で入力してください'),
+  body('password').isLength({ min: 4 }).withMessage('パスワードは4文字以上で入力してください'),
 ]
 
 export const loginValidator = [
   body('email').notEmpty().withMessage('メールアドレスを入力してください'),
   body('email').isEmail().withMessage('メールアドレスの形式が不正です'),
   body('password').notEmpty().withMessage('パスワードを入力してください'),
-  body('password').isLength({ min: 8 }).withMessage('パスワードは8文字以上で入力してください'),
+  body('password').isLength({ min: 4 }).withMessage('パスワードは4文字以上で入力してください'),
 ]
